fa09772e1b4124e3f4d41c57c68c44ec16b56b80
JeroenSverige <77346416+JeroenSverige@users.noreply.github.com>
2023-03-27 16:48:00 +0200

    Create Homepage.php

---
+++ Homepage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hydrofleet</title>
+  </head>
+  <body>
+    <h1>Welcome to Hydrofleet</h1>
+    <p>Hydrofleet is your all-in-one solution for fleet management. Our platform helps you manage your fleet more efficiently, reduce costs, and increase profitability.</p>
+    <h2>Our Services</h2>
+    <ul>
+      <li>Vehicle tracking and monitoring</li>
+      <li>Route optimization and scheduling</li>
+      <li>Fuel management and cost tracking</li>
+      <li>Maintenance and repair scheduling</li>
+    </ul>
+    <h2>Contact Us</h2>
+    <p>If you're interested in learning more about Hydrofleet, please contact us at:</p>
+    <ul>
+      <li>Email: info@hydrofleet.com</li>
+      <li>Phone: 555-1234</li>
+      <li>Address: 123 Main St, Anytown USA</li>
+    </ul>
+  </body>
+</html>
